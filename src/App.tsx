9075d1c6808028ed6d5b32f35d4b3a5bda6b2501
Ramiro Peña <93239263+ramiropen@users.noreply.github.com>
2023-03-14 16:02:37 -0700

    added map function

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,23 @@ function App() {
   ReactGA.initialize("UA-000000-01");
   ReactGA.pageview(window.location.pathname + window.location.search);
 
+  let careers=[{name:"Information Security Analysts", tenthPercentile:61520, average:102600, ninetiethPercentile:165920},
+               {name:"Registered Nurse", tenthPercentile:59450, average:77600, ninetiethPercentile:120250},
+               {name:"Animal Trainers", tenthPercentile:23160, average:31280, ninetiethPercentile:58790} ]
+  
+
   return (
     <div className='App'>
       <h1>My Career Plan</h1>
+      <table>
+      <thead>
+      <tr>
+        <th>Career</th>
+        <th>Average Income</th>
+      </tr>
+      </thead>
+      {careers.map(career=><tr><td>{career.name}</td><td>{career.average}</td></tr>)}
+      </table>
     </div>
   );
 }
