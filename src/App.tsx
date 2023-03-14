19a753f3f51f4a9e07647c329b54c962d6a64ba7
Eric Hoover <ehoover3@yahoo.com>
2023-03-13 21:47:40 -0400

    npm i react-ga

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
+import ReactGA from "react-ga";
 import "./App.css";
 
 function App() {
+  ReactGA.initialize("UA-000000-01");
+  ReactGA.pageview(window.location.pathname + window.location.search);
+
   return (
     <div className='App'>
       <h1>My Career Plan</h1>
