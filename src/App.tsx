a302226d24e367b60229a6e665b4e4cf45c05631
Eric Hoover <ehoover3@yahoo.com>
2023-03-19 21:33:31 -0400

    added feature to sort careers by average national pay

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,32 @@ function App() {
   ReactGA.initialize("UA-000000-01");
   ReactGA.pageview(window.location.pathname + window.location.search);
 
+  const [sortedCareers, setSortedCareers] = useState([...careers]);
+  const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc");
+
+  const handleSortClick = () => {
+    let filterKeepNumbers = careers.filter(
+      (career) => typeof career.salary.national.average == "number"
+    );
+
+    let filterRemoveNumbers = careers.filter(
+      (career) => typeof career.salary.national.average != "number"
+    );
+
+    let sortedCareers = filterKeepNumbers.sort((a: any, b: any) =>
+      sortOrder === "asc"
+        ? a.salary.national.average - b.salary.national.average
+        : b.salary.national.average - a.salary.national.average
+    );
+
+    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+    setSortedCareers(
+      sortOrder === "asc"
+        ? [...filterRemoveNumbers, ...sortedCareers]
+        : [...sortedCareers, ...filterRemoveNumbers]
+    );
+  };
+
   return (
     <div className='App'>
       <h1>My Career Plan</h1>
@@ -15,13 +41,21 @@ function App() {
         <thead>
           <tr>
             <th>Career</th>
-            <th>Average Income</th>
+            <th>
+              <button onClick={() => handleSortClick()}>Average Income</button>
+            </th>
           </tr>
         </thead>
-        {careers.map((career) => (
+        {sortedCareers.map((career) => (
           <tr>
             <td>{career.name}</td>
-            <td>{career.salary.national.average}</td>
+            <td>
+              {career.salary.national.average.toLocaleString("en-US", {
+                style: "currency",
+                currency: "USD",
+                minimumFractionDigits: 0,
+              })}
+            </td>
           </tr>
         ))}
       </table>
