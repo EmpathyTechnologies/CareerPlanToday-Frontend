51a8f6dacf7d3bd8694e72cb4de39098c8c24953
Eric Hoover <ehoover3@yahoo.com>
2023-03-30 18:55:10 -0400

    added plus sign if value is 208,000

--- src/components/Careers/CareersTableRow.tsx
+++ src/components/Careers/CareersTableRow.tsx
@@ -2,10 +2,15 @@ import { formatCurrency } from "../../utilities/formatCurrency";
 export default function CareersTableRow(props: any) {
   let { career } = props;
 
+  function checkIf208000(salary: string) {
+    if (salary === "$208,000") return "$208,000+";
+    else return salary;
+  }
+
   return (
     <tr>
       <td>{career.title}</td>
-      <td>{formatCurrency(career.salary.national.average)}</td>
+      <td>{checkIf208000(formatCurrency(career.salary.national.average))}</td>
     </tr>
   );
 }
