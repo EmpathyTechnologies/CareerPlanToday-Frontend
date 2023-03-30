fc63fce9a07f171b2832eb255990fed87096a554
Eric Hoover <ehoover3@yahoo.com>
2023-03-30 01:43:20 -0400

    commented out colleges temporarily

---
+++ src/components/Colleges/CollegesTableRow.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CollegesTableRow() {
+  return <div>CollegesTableRow</div>;
+}
--- src/pages/Colleges.tsx
+++ src/pages/Colleges.tsx
@@ -1,26 +1,5 @@
-import { useState, useEffect } from "react";
-import colleges from "../data/colleges.json";
-import CollegesNavbar from "../components/Colleges/CollegesNavbar";
-import Footer from "../components/Footer/Footer";
+import React from "react";
 
 export default function Colleges() {
-  const [collegesList, setCollegesList] = useState([...colleges]);
-  const [selected, setSelected] = useState<string>("All Colleges");
-
-  useEffect(() => {
-    if (selected === "All Careers") {
-      setCollegesList([...colleges]);
-    } else {
-      let selectedColleges = colleges.filter((colleges) => colleges.specialties.includes(selected));
-      setCollegesList([...selectedColleges]);
-    }
-  }, [selected]);
-
-  return (
-    <div className='CareerContainer'>
-      <CollegesNavbar selected={selected} setSelected={setSelected} />
-      {/* ADD COLLEGE TABLE & COLLEGE TABLE ITEM COMPONENTS */}
-      <Footer />
-    </div>
-  );
+  return <div>Colleges</div>;
 }
