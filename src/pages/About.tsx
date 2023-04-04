853a5ee490ef7fbaccaf3faedac384dab5dd504e
Ramiro Peña <93239263+ramiropen@users.noreply.github.com>
2023-04-03 19:07:21 -0700

    added a div in about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,6 +4,7 @@ export default function About() {
   return (
     <div>
       <div>This website is brought to you by Yogi, Ramiro, and Eric.</div>
+      <div>This about page was created to inform the public about different job rates.</div>
     </div>
   );
 }
