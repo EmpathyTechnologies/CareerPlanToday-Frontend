b49324e3d8188816bfe3dead4f3936a3c8bddc66
Ramiro Peña <93239263+ramiropen@users.noreply.github.com>
2023-04-04 15:25:54 -0700

    more about changes

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Nav from "react-bootstrap/Nav";
 import Card from "react-bootstrap/Card";
 import img1 from "../Images/about-first.jpg";
-
+import img2 from "../Images/about-second.jpg";
+import img3 from "../Images/about-third.jpg";
 export default function About() {
   return (
     <div>
@@ -12,16 +14,44 @@ export default function About() {
       <hr className="aboutHr"/>
 
       <Card className="about">
-        <Card.Img src={img1} alt="No Pic"/>
+        <Card.Img src={img1} className="aboutImg1" alt="No Pic"/>
         <Card.ImgOverlay>
-        <Card.Text>This website is brought to you by Yogi, Ramiro, and Eric.</Card.Text>
-        <Card.Text>
-          This about website was created to inform the public about different job
-          rates.
-        </Card.Text>
+        <Card.Text className="aboutText">Eric</Card.Text>
         </Card.ImgOverlay>
       </Card> 
-  
+
+      <hr className="aboutHr"/>
+
+      <Card className="about">
+        <Card.Img src={img2} className="aboutImg1" alt="No Pic"/>
+        <Card.ImgOverlay>
+        <Card.Text className="aboutText">Yogi</Card.Text>
+        
+        </Card.ImgOverlay>
+      </Card> 
+
+      <hr className="aboutHr"/>
+
+      <Card className="about">
+        <Card.Img src={img3} className="aboutImg1" alt="No Pic"/>
+        <Card.ImgOverlay>
+        <Card.Text className="aboutText">Ramiro</Card.Text>
+        </Card.ImgOverlay>
+      </Card> 
+
+      <br/>
+
+      <Nav className='BorderTop'>
+      <Nav.Item as='li'>
+        <Nav.Link href='./termsofuse'>Terms</Nav.Link>
+      </Nav.Item>
+      <Nav.Item as='li'>
+        <Nav.Link href='./privacypolicy'>Privacy</Nav.Link>
+      </Nav.Item>
+      <Nav.Item as='li'>
+        <Nav.Link href='./about'>About</Nav.Link>
+      </Nav.Item>
+    </Nav>
 
     </div>
   );
