0bd8fff042a14cf4a335c4ded2788c5713de527f
Ramiro Peña <93239263+ramiropen@users.noreply.github.com>
2023-04-04 18:48:30 -0700

    more changes to about, super fun

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,63 +6,77 @@ import img1 from "../Images/about-first.jpg";
 import img2 from "../Images/about-second.jpg";
 import img3 from "../Images/about-third.jpg";
 import img4 from "../Images/about-fourth.jpg";
+import img5 from "../Images/ramiro-pic.jpg";
+
 export default function About() {
   return (
     <div>
-
       <h1 className="aboutHeader">About Our Team</h1>
 
-      <hr className="aboutHr"/>
+      <hr className="aboutHr" />
 
       <Card className="about">
-        <Card.Img src={img1} className="aboutImg1" alt="No Pic"/>
+        <Card.Img src={img1} className="aboutImg" alt="No Pic" />
         <Card.ImgOverlay>
-        <Card.Text className="aboutText">Eric</Card.Text>
+          <Card.Text className="aboutCardHeader">Eric</Card.Text>
+          {/* <img /> */}
         </Card.ImgOverlay>
-      </Card> 
+      </Card>
 
-      <hr className="aboutHr"/>
+      <hr className="aboutHr" />
 
       <Card className="about">
-        <Card.Img src={img2} className="aboutImg1" alt="No Pic"/>
+        <Card.Img src={img2} className="aboutImg" alt="No Pic" />
         <Card.ImgOverlay>
-        <Card.Text className="aboutText">Yogi</Card.Text>
-        
+          <Card.Text className="aboutCardHeader">Yogi</Card.Text>
         </Card.ImgOverlay>
-      </Card> 
+      </Card>
 
-      <hr className="aboutHr"/>
+      <hr className="aboutHr" />
 
       <Card className="about">
-        <Card.Img src={img3} className="aboutImg1" alt="No Pic"/>
+        <Card.Img src={img3} className="aboutImg" alt="No Pic" />
         <Card.ImgOverlay>
-        <Card.Text className="aboutText">Ramiro</Card.Text>
+          <Card.Text className="aboutCardHeader">Seygourney</Card.Text>
         </Card.ImgOverlay>
-      </Card> 
+      </Card>
 
-      <hr className="aboutHr"/>
+      <hr className="aboutHr" />
 
       <Card className="about">
-        <Card.Img src={img4} className="aboutImg1" alt="No Pic"/>
+        <Card.Img src={img4} className="aboutImg" alt="No Pic" />
         <Card.ImgOverlay>
-        <Card.Text className="aboutText">Seygourney</Card.Text>
+          <Card.Text className="aboutCardHeader">Ramiro</Card.Text>
+          <img className="portrait" src={img5} alt="no pic" />
+          <Card.Text className="aboutText">
+            I contribute as a skilled software developer with problem-solving
+            abilities and attention to detail. My expertise in writing code and
+            developing functional software solutions is essential to achieving
+            the team's goals. I collaborate effectively, share my knowledge and
+            expertise, and foster a culture of learning and growth. In addition,
+            I highly value comradery within the team, and I believe that a
+            positive and collaborative work environment is crucial for success.
+            I am always willing to lend a helping hand and work together with my
+            colleagues to achieve our common goals. As a team player, I strive
+            to create an atmosphere of mutual respect and support, which allows
+            us to work more efficiently and achieve our objectives effectively.
+          </Card.Text>
         </Card.ImgOverlay>
-      </Card> 
+      </Card>
 
-      <br/>
+      <br />
 
-      <Nav className='BorderTop'>
-      <Nav.Item as='li'>
-        <Nav.Link href='./termsofuse'>Terms</Nav.Link>
-      </Nav.Item>
-      <Nav.Item as='li'>
-        <Nav.Link href='./privacypolicy'>Privacy</Nav.Link>
-      </Nav.Item>
-      <Nav.Item as='li'>
-        <Nav.Link href='./about'>About</Nav.Link>
-      </Nav.Item>
-    </Nav>
-
+      <Nav className="BorderTop">
+        <Nav.Item as="li">
+          <Nav.Link href="./termsofuse">Terms</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link href="./privacypolicy">Privacy</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link href="./about">About</Nav.Link>
+        </Nav.Item>
+      </Nav>
     </div>
   );
 }
