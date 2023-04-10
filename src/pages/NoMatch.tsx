d4ef74a82bb5edfda31ebdd0ea8eceb89d0d40e4
yogevzipori <yogevzipori@gmail.com>
2023-04-10 16:24:25 -0700

    removing comments

--- src/pages/NoMatch.tsx
+++ src/pages/NoMatch.tsx
@@ -1,38 +1,3 @@
-// import Footer from "../layouts/Footer";
-// import Button from "react-bootstrap/Button";
-// import NotFound from "../assets/notfound1.jpg";
-// import { Link } from "react-router-dom";
-// import Figure from "react-bootstrap/Figure";
-
-// export default function NoMatch(props: any) {
-//   return (
-//     <div>
-//       <h1>Whoops...</h1>
-//       <h2>This is the wrong career path for you...</h2>
-//       <Figure.Caption>
-//         <Link to="/">
-//           <div>
-//             <h3>Click on this link to continue.</h3>
-//           </div>
-//         </Link>
-//       </Figure.Caption>
-//       <div className="text-center">
-//         <Figure>
-//           <Figure.Image
-//             width={871}
-//             height={580}
-//             alt="Page Not Found"
-//             src={NotFound}
-//           />
-//           <Button variant="contained" size="lg" href="/" style={{ marginTop: '1rem' }}>
-//             Back to All Careers
-//           </Button>
-//         </Figure>
-//       </div>
-//       <Footer />
-//     </div>
-//   );
-// }
 
 import React from "react";
 import Footer from "../layouts/Footer";
