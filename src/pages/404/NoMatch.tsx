41a12f5e0b431b611e109656ec9967d9d5110da4
yogevzipori <yogevzipori@gmail.com>
2023-04-13 21:41:03 -0700

    deleted comments

--- src/pages/404/NoMatch.tsx
+++ src/pages/404/NoMatch.tsx
@@ -1,35 +1,4 @@
 
-// import Footer from "../../layouts/Footer";
-// import Button from "react-bootstrap/Button";
-// import NotFound from "../../assets/notfound1.jpg";
-// import { Link } from "react-router-dom";
-// import Figure from "react-bootstrap/Figure";
-// import styles from "../../NoMatch.module.css";
-// import Page1NotFound from "./Page1NotFound";
-// import Page2NotFound from "./Page2NotFound";
-
-
-// export default function NoMatch(props: any) {
-//   const random404Components = [Page1NotFound, Page2NotFound];
-
-//   return (
-//     <div>
-//   {      const randomPage = random404Components[Math.floor(Math.random() * random404Components.length)];
-// }     <randomPage />
-
-
-//     </div>
-//   );
-// }
-
-
-// import React from "react";
-// import Footer from "../../layouts/Footer";
-// import Button from "react-bootstrap/Button";
-// import NotFound from "../../assets/notfound1.jpg";
-// import { Link } from "react-router-dom";
-// import Figure from "react-bootstrap/Figure";
-// import styles from "../../NoMatch.module.css";
 import Page1NotFound from "./Page1NotFound";
 import Page2NotFound from "./Page2NotFound";
 import Page3NotFound from "./Page3NotFound";
