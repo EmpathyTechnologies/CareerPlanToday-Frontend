42334ed0fd3eaaa6c574a2dffef3d972c928bf5b
Ramiro Peña <93239263+ramiropen@users.noreply.github.com>
2023-04-16 19:18:25 -0700

    some more changes to about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -8,43 +8,48 @@ import Footer from "../layouts/Footer";
 export default function About() {
   const people = [
     {
-      name: "Ramiro",
       img: ramiroPic,
-      description:
-        "I contribute as a skilled software developer with problem-solving abilities and attention to detail. My expertise in writing code and developing functional software solutions is essential to achieving the team's goals. I collaborate effectively, share my knowledge and expertise, and foster a culture of learning and growth. In addition, I highly value comradery within the team, and I believe that a positive and collaborative work environment is crucial for success. I am always willing to lend a helping hand and work together with my colleagues to achieve our common goals. As a team player, I strive to create an atmosphere of mutual respect and support, which allows us to work more efficiently and achieve our objectives effectively.",
+      name: "Ramiro",
+      position:"Software Developer",
+      linkedIn: "https://www.linkedin.com/in/ramiro-pena/",
     },
     {
-      name: "Yogi",
       img: yogiPic,
-      description: "",
+      name: "Yogi",
+      position:"Software Developer",
+      linkedIn: "https://www.linkedin.com/in/yogi-zipori/",
     },
     {
-      name: "Mae",
       img: maePic,
-      description: "",
+      name: "Mae",
+      position:"Software Developer",
+      linkedIn: "https://www.linkedin.com/in/maepena/",
     },
     {
-      name: "Eric",
       img: ericPic,
-      description:
-        "Eric's leadership skills and technical expertise as a skilled software developer allowed him to bring together a diverse team to help others find employment. He motivated the team to work collaboratively and create innovative solutions using their individual talents and perspectives. With Eric's guidance, the team was able to develop user-friendly websites and mobile applications that made job searching more accessible. Eric's leadership and technical proficiency were a winning combination that made a real difference in the lives of job seekers.",
+      name: "Eric",
+      position:"Software Developer",
+      linkedIn: "https://www.linkedin.com/in/hoov/",
     },
   ];
-
-  const peopleList = people.map(person => (
-    <div key={person.name}>
-      <h2>{person.name}</h2>
-      <img src={person.img} alt={person.name} />
-      <p>{person.description}</p>
-    </div>
-  ));
-
-  return (
-    <div>
-      <h1>About Us</h1>
-      {peopleList}
-    </div>
-  );
-}
-
+  
+    const cardsList = people.map(person => (
+      <Card style={{ width: '18rem' }} key={person.name}>
+        <Card.Img variant="top" src={person.img}/>
+        <Card.Body>
+          <Card.Title>{person.name}</Card.Title>
+          <Card.Text>{person.position}</Card.Text>
+          <Card.Link href={person.linkedIn}>LinkedIn</Card.Link>
+        </Card.Body>
+      </Card>
+    ));
+  
+    return (
+      <div>
+        <h1 className="aboutHeader">About Us</h1>
+        <div className="aboutCardContainer">{cardsList}</div>
+        <Footer />
+      </div>
+    );
+  }
 
