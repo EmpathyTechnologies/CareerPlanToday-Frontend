bc7135519079cd2a3cda9cb04399c7b37ddd0e94
Eric Hoover <ehoover3@yahoo.com>
2023-05-04 22:50:45 -0400

    created a fullstack feedback component

--- src/pages/Feedback.tsx
+++ src/pages/Feedback.tsx
@@ -1,26 +1,69 @@
 import { useState, useEffect } from "react";
 
-export default function Feedback() {
-  const [testFetchGetUser, setTestFetchGetUser] = useState("test for user");
+interface FeedbackInterface {
+  id: string;
+  message: string;
+}
+
+function generateUniqueId(): string {
+  const now: Date = new Date();
+  const uniqueId: string = `${now.getFullYear()}-${padZero(now.getMonth() + 1)}-${padZero(now.getDate())}_${padZero(now.getHours())}:${padZero(
+    now.getMinutes()
+  )}:${padZero(now.getSeconds())}.${padZero(now.getMilliseconds())}`;
+  return uniqueId;
+}
+
+function padZero(num: number): string {
+  return num.toString().padStart(2, "0");
+}
+
+function Feedback() {
+  const [feedbackList, setFeedbackList] = useState<FeedbackInterface[]>([]);
+  const [newFeedback, setNewFeedback] = useState("");
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("https://2esc5vgt4m.execute-api.us-east-1.amazonaws.com/dev/api/getuser");
-        const data = await response.json();
-        console.log(data);
-        setTestFetchGetUser(data.UserName);
-      } catch (error) {
-        console.log("ERROR: ", error);
-        setTestFetchGetUser("Error");
-      }
-    };
-    fetchData();
+    fetch("https://qsv6ogegh7.execute-api.us-east-1.amazonaws.com/production/feedback")
+      .then((response) => response.json())
+      .then((data: FeedbackInterface[]) => setFeedbackList(data));
   }, []);
+
+  const handleSubmitFeedback = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    fetch("https://qsv6ogegh7.execute-api.us-east-1.amazonaws.com/production/feedback", {
+      method: "PUT",
+      body: JSON.stringify({ id: generateUniqueId(), message: newFeedback }),
+    })
+      .then((response) => response.json())
+      .then((data: FeedbackInterface) => {
+        setNewFeedback("");
+        setFeedbackList((prevState) => [...prevState, data]);
+      });
+  };
+
+  const handleDeleteFeedback = (id: string) => {
+    fetch(`https://qsv6ogegh7.execute-api.us-east-1.amazonaws.com/production/feedback/${id}`, {
+      method: "DELETE",
+    }).then(() => {
+      setFeedbackList((prevState) => prevState.filter((feedback) => feedback.id !== id));
+    });
+  };
 
   return (
     <div>
-      <div>{testFetchGetUser}</div>
+      <h1>Feedback App</h1>
+      <form onSubmit={handleSubmitFeedback}>
+        <input type='text' value={newFeedback} onChange={(e) => setNewFeedback(e.target.value)} />
+        <button type='submit'>Submit</button>
+      </form>
+      <ul>
+        {feedbackList.map((feedback) => (
+          <li key={feedback.id}>
+            {feedback.message} <button onClick={() => handleDeleteFeedback(feedback.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+export default Feedback;
