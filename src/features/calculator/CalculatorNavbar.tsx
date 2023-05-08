e6b856070a9e9ac0859988b68c4095d79dbccb2e
Eric Hoover <ehoover3@yahoo.com>
2023-05-08 00:13:06 -0400

    updated calculator navbar

--- src/features/calculator/CalculatorNavbar.tsx
+++ src/features/calculator/CalculatorNavbar.tsx
@@ -3,9 +3,10 @@ export default function calculatorNavbar() {
   return (
     <div className='CalculatorNavbar'>
       {/* ToDo - Make these drop downs with favorites at the top */}
-      <span>MyCareer &nbsp;</span>
-      <span>MyCollege &nbsp;</span>
-      <span>MyHomeState &nbsp;</span>
+      <span>CareerWithCollege &nbsp;</span>
+      <span>CareerNoCollege &nbsp;</span>
+      <span>College &nbsp;</span>
+      <span>HomeState &nbsp;</span>
       <span>LiveOnCampus</span>
     </div>
   );
