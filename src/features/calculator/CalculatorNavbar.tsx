9e6862ff7240d87a4a57286d481405cd1bef8d35
Eric Hoover <ehoover3@yahoo.com>
2023-05-08 00:23:42 -0400

    updated calculator navbar

--- src/features/calculator/CalculatorNavbar.tsx
+++ src/features/calculator/CalculatorNavbar.tsx
@@ -1,13 +1,39 @@
 import "./CalculatorNavbar.css";
+import { Dropdown, DropdownButton } from "react-bootstrap";
+
 export default function calculatorNavbar() {
   return (
     <div className='CalculatorNavbar'>
       {/* ToDo - Make these drop downs with favorites at the top */}
-      <span>CareerWithCollege &nbsp;</span>
-      <span>CareerNoCollege &nbsp;</span>
-      <span>College &nbsp;</span>
-      <span>HomeState &nbsp;</span>
-      <span>LiveOnCampus</span>
+      <DropdownButton title='Career with College'>
+        <Dropdown.Item href='#/action-1'>Action</Dropdown.Item>
+        <Dropdown.Item href='#/action-2'>Another action</Dropdown.Item>
+        <Dropdown.Item href='#/action-3'>Something else</Dropdown.Item>
+      </DropdownButton>
+      &nbsp;
+      <DropdownButton title='Career without College'>
+        <Dropdown.Item href='#/action-1'>Action</Dropdown.Item>
+        <Dropdown.Item href='#/action-2'>Another action</Dropdown.Item>
+        <Dropdown.Item href='#/action-3'>Something else</Dropdown.Item>
+      </DropdownButton>
+      &nbsp;
+      <DropdownButton title='College'>
+        <Dropdown.Item href='#/action-1'>Action</Dropdown.Item>
+        <Dropdown.Item href='#/action-2'>Another action</Dropdown.Item>
+        <Dropdown.Item href='#/action-3'>Something else</Dropdown.Item>
+      </DropdownButton>
+      &nbsp;
+      <DropdownButton title='Home State'>
+        <Dropdown.Item href='#/action-1'>Action</Dropdown.Item>
+        <Dropdown.Item href='#/action-2'>Another action</Dropdown.Item>
+        <Dropdown.Item href='#/action-3'>Something else</Dropdown.Item>
+      </DropdownButton>
+      &nbsp;
+      <DropdownButton title='Live On Campus'>
+        <Dropdown.Item href='#/action-1'>Action</Dropdown.Item>
+        <Dropdown.Item href='#/action-2'>Another action</Dropdown.Item>
+        <Dropdown.Item href='#/action-3'>Something else</Dropdown.Item>
+      </DropdownButton>
     </div>
   );
 }
