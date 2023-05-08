fb2658feb58b07a10f0afdfe8817617c22e00cd7
Eric Hoover <ehoover3@yahoo.com>
2023-05-07 22:39:43 -0400

    changed calculator colors

--- src/features/colleges/components/CollegesNavbar/CollegesNavbar.tsx
+++ src/features/colleges/components/CollegesNavbar/CollegesNavbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./CollegesNavbar.css";
 import CollegesFilterByStates from "./CollegesFilterByStates";
 import CollegesFilterByTuition from "./CollegesFilterByTuition";
