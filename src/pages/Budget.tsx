99eb945fd137f36c4725309c1e42db60b1b747b5
Eric Hoover <ehoover3@yahoo.com>
2023-05-08 00:08:14 -0400

    made navbar for calculator page

--- src/pages/Budget.tsx
+++ src/pages/Budget.tsx
@@ -1,82 +1,102 @@
 import React from "react";
+import { Container, Row, Col, Form } from "react-bootstrap";
 
 export default function Budget() {
   let displayFlex = { display: "flex", justifyContent: "space-between" };
 
   return (
     <div>
-      <div>After College Budget</div>
-      <div style={{ fontWeight: "bold" }}>INCOME</div>
-      <div style={displayFlex}>
-        <div>Average Salary</div>
-        <input type='text' />
-      </div>
-      <div style={displayFlex}>
-        <div>Estimated Taxes</div>
-        <input type='text' />
-      </div>
-      <div style={displayFlex}>
-        <div>Monthly Take-home</div>
-        <input type='text' />
-      </div>
-      <div style={displayFlex}>
-        <div>Other Monthly Income</div>
-        <input type='text' />
-      </div>
-      <div style={displayFlex}>
-        <div style={{ fontWeight: "bold" }}>Total Monthly Income</div>
-        <input type='text' />
-      </div>
+      <h3>After College Budget</h3>
+
+      <h4>INCOME</h4>
+      <Row>
+        <Col xs={8}>Average Salary</Col>
+        <Col xs={4}>
+          <Form.Control type='text' />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={8}>Estimated Taxes</Col>
+        <Col xs={4}>
+          <Form.Control type='text' />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={8}>Monthly Take-home</Col>
+        <Col xs={4}>
+          <Form.Control type='text' />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={8}>Other Monthly Income</Col>
+        <Col xs={4}>
+          <Form.Control type='text' />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={8}>
+          <strong>Total Monthly Income</strong>
+        </Col>
+        <Col xs={4}>
+          <Form.Control type='text' />
+        </Col>
+      </Row>
       <div style={{ fontWeight: "bold" }}>EXPENSES (per Monthl)</div>
 
-      <div style={displayFlex}>
-        <div>Housing</div>
-        <select name='housing'>
+      <Form.Group controlId='housing'>
+        <Form.Label>Housing</Form.Label>
+        <Form.Control as='select'>
           <option value='1200'>Live Alone ($1200)</option>
           <option value='600'>Roommate ($600)</option>
           <option value='250'>Live with Relatives ($250)</option>
-        </select>
-      </div>
-      <div>
-        <div>Transport</div>
-        <select name='housing'>
+        </Form.Control>
+      </Form.Group>
+
+      <Form.Group controlId='transport'>
+        <Form.Label>Transport</Form.Label>
+        <Form.Control as='select'>
           <option value='750'>New Car ($700)</option>
           <option value='325'>Used Car ($325)</option>
           <option value='150'>Public Transportation ($150)</option>
-        </select>
-      </div>
-      <div>
-        <div>Food</div>
-        <select name='housing'>
+        </Form.Control>
+      </Form.Group>
+
+      <Form.Group controlId='food'>
+        <Form.Label>Food</Form.Label>
+        <Form.Control as='select'>
           <option value='1000'>Restaurants ($1000)</option>
           <option value='750'>Fast Food ($750)</option>
           <option value='500'>Groceries ($500)</option>
-        </select>
-      </div>
-      <div>
-        <div>Entertainment</div>
-        <select name='housing'>
+        </Form.Control>
+      </Form.Group>
+
+      <Form.Group controlId='entertainment'>
+        <Form.Label>Entertainment</Form.Label>
+        <Form.Control as='select'>
           <option value='1200'>Live Alone ($1200)</option>
           <option value='600'>Roommate ($600)</option>
-          <option value='250'>Live with Relatives ($250)</option>
-        </select>
-      </div>
-      <div>
-        <div>Pets</div>
-        <select name='housing'>
+          <option value='250'>Live with Relatives ($250)</option>{" "}
+        </Form.Control>
+      </Form.Group>
+
+      <Form.Group controlId='pets'>
+        <Form.Label>Pets</Form.Label>
+        <Form.Control as='select'>
           <option value='1200'>2 Pets ($500)</option>
           <option value='600'>1 Pet ($250)</option>
           <option value='250'>No Pets ($0)</option>
-        </select>
-      </div>
-      <div>
-        <div>Kids</div>
-        <select name='housing'>
+        </Form.Control>
+      </Form.Group>
+
+      <Form.Group controlId='kids'>
+        <Form.Label>Kids</Form.Label>
+        <Form.Control as='select'>
           <option value='1200'>Live Alone ($1200)</option>
           <option value='600'>Roommate ($600)</option>
-          <option value='250'>Live with Relatives ($250)</option>
-        </select>
-      </div>
+          <option value='250'>Live with Relatives ($250)</option>{" "}
+        </Form.Control>
+      </Form.Group>
+
       <div>
         <div>Other</div>
         <div>Insurance</div>
@@ -91,25 +111,13 @@ export default function Budget() {
 
       <div>
         <div>Total Monthly Income</div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
       </div>
 
       <div>
         <div>Total Monthly Expenses</div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
       </div>
       <div>
         <div>Net Income (Loss)</div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
       </div>
     </div>
   );
