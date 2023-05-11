d3e18fa0458f25a0ac70376b018c4fc9a18076f6
Eric Hoover <ehoover3@yahoo.com>
2023-05-10 23:13:38 -0400

    removed cookies

--- src/features/careers/components/CareersTable/CareersTableBody.tsx
+++ src/features/careers/components/CareersTable/CareersTableBody.tsx
@@ -1,12 +1,19 @@
 import CareersTableRow from "./CareersTableRow";
 
 export default function CareersTableBody(props: any) {
-  let { sortedCareers, favorites, setFavorites } = props;
+  let {
+    sortedCareers,
+    // favorites, setFavorites
+  } = props;
 
   return (
     <tbody>
       {sortedCareers.map((career: any) => (
-        <CareersTableRow key={career.title} career={career} favorites={favorites} setFavorites={setFavorites} />
+        <CareersTableRow
+          key={career.title}
+          career={career}
+          // favorites={favorites} setFavorites={setFavorites}
+        />
       ))}
     </tbody>
   );
