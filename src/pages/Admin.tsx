41f24a7913112252289c08186089e4cb32e5a824
Eric Hoover <ehoover3@yahoo.com>
2023-05-11 09:15:49 -0400

    added navbar-spacer classname to admin page

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -3,5 +3,5 @@ import React from "react";
 // Tutorial https://www.youtube.com/watch?v=yKV1IGahXqA
 
 export default function Admin() {
-  return <div>Admin</div>;
+  return <div className='navbar-spacer'>Admin</div>;
 }
