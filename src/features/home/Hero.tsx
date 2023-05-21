ea9d60e72cd1b3b6026b85744e2564cf5ba24eef
Eric Hoover <ehoover3@yahoo.com>
2023-05-21 10:48:08 -0400

    updated page

--- src/features/home/Hero.tsx
+++ src/features/home/Hero.tsx
@@ -4,13 +4,18 @@ import "../../assets/styles/Hero.css";
 function Hero() {
   return (
     <div className='hero-container' style={{ backgroundColor: "#1cb0f6" }}>
-      <h1>Design Your Better Tomorrow Today</h1>
+      <h1>Navigate your future today</h1>
+      <p>Explore careers, compare colleges, and form your plan</p>
+
       <div className='hero-btns'>
-        <Button variant='success' className='btns'>
+        <Button variant='success' className='btns' href='./careers'>
           EXPLORE CAREERS
         </Button>
-        <Button variant='success' className='btns'>
+        <Button variant='success' className='btns' href='./colleges'>
           COMPARE COLLEGES
+        </Button>
+        <Button variant='success' className='btns' href='./plan'>
+          FORM YOUR PLAN
         </Button>
       </div>
     </div>
