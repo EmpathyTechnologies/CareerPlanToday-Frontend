6afabc680b532af7b3a6628ac5cf76b109773e41
Eric Hoover <ehoover3@yahoo.com>
2023-06-17 16:25:59 -0400

    added link to plan buttons

--- src/pages/Plan.tsx
+++ src/pages/Plan.tsx
@@ -1,28 +1,41 @@
+import React from "react";
 import Button from "../components/Button";
 import Card from "react-bootstrap/Card";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import { useNavigate } from "react-router-dom";
+
+interface CardItem {
+  step: number;
+  title: string;
+  btnText: string;
+  href: string;
+}
 
 function Plan() {
-  let myCards = [
-    { step: 1, title: "Incomplete", btnText: "Explore Careers", href: "./careers" },
-    { step: 2, title: "Incomplete", btnText: "Compare Colleges", href: "./colleges" },
+  let myCards: CardItem[] = [
+    { step: 1, title: "Incomplete", btnText: "Explore Careers", href: "/careers" },
+    { step: 2, title: "Incomplete", btnText: "Compare Colleges", href: "/colleges" },
     { step: 3, title: "Incomplete", btnText: "How to Fund College", href: "" },
     { step: 4, title: "Incomplete", btnText: "Choose your lifestyle", href: "" },
     { step: 5, title: "Incomplete", btnText: "See How You Can Invest", href: "" },
   ];
 
-  // TODO: Add button onclick functionality to redirect to the desired hyperlink
+  const navigate = useNavigate();
+
+  const handleButtonClick = (href: string) => {
+    if (href) {
+      navigate(href);
+    }
+  };
 
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
       {myCards.map((card) => (
-        <div style={{ display: "flex", alignItems: "center" }}>
+        <div style={{ display: "flex", alignItems: "center" }} key={card.step}>
           <h2>Step {card.step}</h2>
           <Card>
             <Card.Body>
               <Card.Title>{card.title}</Card.Title>
-              <Button text={card.btnText} color='btn-blue' />
+              <Button text={card.btnText} color='btn-blue' onClick={() => handleButtonClick(card.href)} />
             </Card.Body>
           </Card>
         </div>
