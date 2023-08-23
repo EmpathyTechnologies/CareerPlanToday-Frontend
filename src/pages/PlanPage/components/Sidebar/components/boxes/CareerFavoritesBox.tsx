a0729aa3076be274d581071ea52b65e5a82fbb02
ehoover3 <ehoover3@yahoo.com>
2023-08-23 15:17:22 -0400

    updated accordion counters

--- src/pages/PlanPage/components/Sidebar/components/boxes/CareerFavoritesBox.tsx
+++ src/pages/PlanPage/components/Sidebar/components/boxes/CareerFavoritesBox.tsx
@@ -1,35 +1,52 @@
-import { Divider, List, Box, ListItem, ListItemText } from "@mui/material";
-
-import CareerTabItem from "../tabs/CareerTabItem";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Divider,
+  Typography,
+} from "@mui/material";
 import { RootState } from "../../../../../../redux/store";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import BlankTab from "../tabs/BlankTab";
+import CareerTabItem from "../tabs/CareerTabItem";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 function CareerFavoritesBox({ itemCss }: any) {
   const savedCareers = useSelector((state: RootState) => state.careers);
 
   return (
-    <Box sx={{ bgcolor: "#101F33" }}>
-      <ListItem sx={{ py: 2, px: 3 }}>
-        <ListItemText sx={{ color: "#fff" }}>Favorite Careers</ListItemText>
-      </ListItem>
-      {savedCareers.length > 0 ? (
-        savedCareers.map((career) => (
-          <CareerTabItem
-          careerData={career}
-            itemCss={itemCss}
-            salary={career.salary}
-            title={career.title}
-            key={career.title}
-          />
-        ))
-      ) : (
-        <BlankTab   text={"careers"}          itemCss={itemCss}
-        />
-      )}
-      <Divider sx={{ mt: 2 }} />
-    </Box>
+    <Accordion sx={{ bgcolor: "#101F33", color: "#fff" }}>
+      <AccordionSummary
+        expandIcon={
+          <ExpandMoreIcon sx={{ bgcolor: "#101F33", color: "#fff" }} />
+        }
+        aria-controls="career-favorites-content"
+        id="career-favorites-header"
+      >
+        <Typography>{savedCareers.length} Favorite Careers</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Box sx={{ bgcolor: "#101F33" }}>
+          {savedCareers.length > 0 ? (
+            savedCareers.map((career) => (
+              <CareerTabItem
+                careerData={career}
+                itemCss={itemCss}
+                salary={career.salary}
+                title={career.title}
+                key={career.title}
+              />
+            ))
+          ) : (
+            <BlankTab text={"careers"} itemCss={itemCss} />
+          )}
+          <Divider sx={{ mt: 2 }} />
+        </Box>
+      </AccordionDetails>
+    </Accordion>
   );
 }
 
 export default CareerFavoritesBox;
+
