8bd9c2c4131d550987909590ef9723d62619047f
root <mtrhoades0727@gmail.com>
2023-08-28 15:59:42 -1000

    commtting for my pull

--- src/pages/InvestPage/InvestPage.tsx
+++ src/pages/InvestPage/InvestPage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Form, Container, Row, Col } from "react-bootstrap";
 import Navigation from "../../layouts/Navigation";
 import Footer from "../../layouts/Footer";
